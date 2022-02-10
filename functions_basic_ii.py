bf03237e71511d200cc073448db1ed72fa7834bb
TerryChai <email@gmail.com>
2022-02-09 18:24:58 -0800

    first commit

---
+++ functions_basic_ii.py
@@ -0,0 +1,64 @@
+#Countdown
+"""
+1. create a function that accepts a number as a an input
+2. return a new list that countsdown by a value of one
+"""
+#i = 6
+#while i > 0:
+#    print(i)
+#    i = i - 1
+#else:
+#     print(" ")
+
+#Print and Return 
+"""
+1. create a function that recieves a list with two numbers
+2. print the first value and return the second value
+"""
+# def return_multiple_values():
+#    return [1,2]
+
+
+#print(return_multiple_values())
+
+#First Plus Length
+"""
+1. create a function that accepts a list
+2. return the sum of the first value in the list plus the list's length
+"""
+# def first_plus_length(list):
+#     print(list[0])
+#     return len(list)
+# print(first_plus_length([3, 3, 10]))
+
+#Values Greater than Second
+'''
+1. create a function that accepts a list and creates a new list containing only the values from the original list that is greater than its 2nd value
+2. 
+'''
+#    [0, 1, 2, 3, 4]
+
+# x = [5,2,3,2,1,4]
+# def randomList(my_list):
+#     numList = []
+#     for i in range(len(my_list)):
+#         # print(i, my_list[i])
+#         if my_list[i]>my_list[1]:
+#             numList.append(my_list[i])
+#     print(numList)
+# randomList(x)
+
+#This Length, That Value
+"""
+1. create a function that accepts two integers as parameters
+2. give the parameters a size and value
+3. 
+"""
+
+# def length_and_value(length, value):
+#     ranNum = []
+#     for i in range(length):
+#         ranNum.append(value)
+#     return ranNum
+# print(length_and_value(4,7))
+# print(length_and_value(6,2))
